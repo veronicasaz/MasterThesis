055c0ad8bca02fc706b8aca21cfcc0d9209f2d85
veronicasaz <veronica.saz.ulibarrena@gmail.com>
2020-09-07 12:19:21 +0200

    Lambert short test, low precision

--- GenerateTrainingDataFromOpt.py
+++ GenerateTrainingDataFromOpt.py
@@ -183,7 +183,7 @@ class Fitness:
         # Feasibility
         # if np.linalg.norm(self.Error[0:3]) <= 100e3 and \
         #     np.linalg.norm(self.Error[3:]) <= 100: # Feasible trajectory
-        if np.linalg.norm(self.Error[0:3]) <= 1e7 and \
+        if np.linalg.norm(self.Error[0:3]) <= 5e7 and \
             np.linalg.norm(self.Error[3:]) <= 5e3: # TODO: change. Too much
             feasible = 1
         else:
@@ -296,8 +296,8 @@ if __name__ == "__main__":
     ####################
     # CREATION OF RANDOM POPULATION
     ####################
-    nsamples = 10 # number of training samples. TODO: increase
-    sample_inputs = np.zeros((nsamples, len(bnds)))
+    nsamples = 500 # number of training samples. TODO: increase
+    samples_Lambert = np.zeros((nsamples, len(bnds)))
     # for decv in range(len(bnds)): 
     #     sample_inputs[:, decv] = np.random.uniform(low = bnds[decv][0], \
     #         high = bnds[decv][1], size = nsamples)
@@ -305,41 +305,57 @@ if __name__ == "__main__":
     ####################
     # CREATION OF RANDOM POPULATION WITH LAMBERT
     ####################
+    start_time = time.time()
+
     for decv in range(6,8): 
-        sample_inputs[:, decv] = np.random.uniform(low = bnds[decv][0], \
+        samples_Lambert[:, decv] = np.random.uniform(low = bnds[decv][0], \
             high = bnds[decv][1], size = nsamples)
     
     # Lambert for calculation of the velocity vectors 
     earthephem = pk.planet.jpl_lp('earth')
     marsephem = pk.planet.jpl_lp('mars')
+
+    notvalid = list()
     for i in range(nsamples):
-        t_0 = sample_inputs[i, 6]
-        t_t = sample_inputs[i, 7]
+        t_0 = samples_Lambert[i, 6]
+        t_t = samples_Lambert[i, 7]
 
         r_0, vE = earthephem.eph(t_0)
         r_1, vM = marsephem.eph(t_0 + AL_BF.sec2days(t_t))
 
-        
-        lambert = AL_TR.Lambert(np.array(r_0), np.array(r_1), t_t, Fitness.sun.mu)
-        v1_h, v2_h = lambert.TerminalVelVect()
-        v1 = v1_h- vE
-        v2 = v2_h- vM
+        nrevs = 2
+        l = pk.lambert_problem(r1 = r_0, r2 = r_1, tof = t_t, cw = False, mu = pk.MU_SUN, max_revs=nrevs)
+        v1 = np.array(l.get_v1())
+        v2 = np.array(l.get_v2())
 
-        print("velocity", v1, v1_h,v2)
-        print(bnds[0:6])
-        for bound in range(3):
-            if v1[bound] >= bnds[bound][0] and  v1[bound] <= bnds[bound][1] and \
-               v2[bound] >= bnds[bound+3][0] and  v2[bound] <= bnds[bound+3][1]:
-                None
-            else:
-                sample_inputs[i,:] = np.zeros(len(bnds))
-                # set all to zero to recognise them
+        # Check if any of the solutions for the revolutions has velocities
+        # within the bounds
+        nrevs_valid = -1
+        for rev in range(len(v1)):
+            v_i = np.linalg.norm(v1[rev] - np.array(vE)) # Relative velocities for the bounds 
+            v_i2 = np.linalg.norm(v2[rev] - np.array(vM))
+            # Change to polar for the bounds
+            if v_i >= bnds[0][0] and  v_i <= bnds[0][1] and \
+            v_i2 >= bnds[3][0] and  v_i2 <= bnds[3][1]:
+                
+                samples_Lambert[i, 0:3] = AL_BF.convert3dvector(v1[rev]-vE, "cartesian")
+                samples_Lambert[i, 3:6] = AL_BF.convert3dvector(v2[rev]-vM, "cartesian")
 
-        sample_inputs[i, 0:3] = AL_BF.convert3dvector(v1, "cartesian")
-        sample_inputs[i, 3:6] = AL_BF.convert3dvector(v2, "cartesian")
+                break
+            elif rev == len(v1)-1:
+                notvalid.append(i)
+                # sample_inputs[i,:] = np.zeros(len(bnds))
 
+        ####################
+        # Evaluate similarity between lambert and propagated trajectory
+        ####################
+                
+    # Delete not valid rows:
+    sample_inputs = np.delete(samples_Lambert, notvalid, axis = 0)
 
-        # if velocity out of bounds it should be eliminated in the optimization
+    t = (time.time() - start_time) 
+    print("Samples", nsamples, "Non valid", len(notvalid))
+    print("Time for Lambert", t)
 
 
     ####################
@@ -348,12 +364,12 @@ if __name__ == "__main__":
     # find feasible trajectories
     # Only the initial and the optimized trajectory will be saved
     ####################
-    niter = 20
+    niter = 10
     niterlocal = 100
-    niter_success = 20
+    niter_success = 10
     mytakestep = AL_OPT.MyTakeStep(Nimp, bnds)
     
-    for i_sample in range(nsamples):
+    for i_sample in range(len(sample_inputs)):
         print("-------------------------------")
         print("Sample %i"%i_sample)
         print("-------------------------------")
