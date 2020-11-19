bbaf21439827d1ae64b24f56a01256d16afe72a2
veronicasaz <veronica.saz.ulibarrena@gmail.com>
2020-11-19 22:27:55 +0100

    Plot epochs vs time

--- NNOpt_reg.py
+++ NNOpt_reg.py
@@ -123,13 +123,13 @@ def optTra(perceptron, dv_SP, dv_RP, dv_EP):
     test_accuracy_Matrix_train2 = np.zeros((len(dv_EP), 2 ))
     dv0 = 5
     dv1 = 40
-    dv4 = 100
-    for i, dv2 in enumerate(dv_SP):
-        for j, dv3 in enumerate(dv_RP):
-            print("OPT train", dv2, dv3)
-            dv = [dv0, dv1, dv2, dv3, dv4]
-            test_accuracy = perceptron.training(dv)
-            test_accuracy_Matrix_train[i, j] = test_accuracy
+    # dv4 = 100
+    # for i, dv2 in enumerate(dv_SP):
+    #     for j, dv3 in enumerate(dv_RP):
+    #         print("OPT train", dv2, dv3)
+    #         dv = [dv0, dv1, dv2, dv3, dv4]
+    #         test_accuracy = perceptron.training(dv)
+    #         test_accuracy_Matrix_train[i, j] = test_accuracy
 
     dv2 = 5
     dv3 = 2
@@ -144,13 +144,13 @@ def optTra(perceptron, dv_SP, dv_RP, dv_EP):
                 
     FileName2 =  "./Results/TrainingPopulation/NNoptimization_reg_train_test.txt"
     FileName3 =  "./Results/TrainingPopulation/NNoptimization_reg_train_test2.txt"
-    mat = np.matrix(test_accuracy_Matrix_train) # cannot save 3d matrix
+    # mat = np.matrix(test_accuracy_Matrix_train) # cannot save 3d matrix
     mat2 = np.matrix(test_accuracy_Matrix_train2)
 
-    with open(FileName2, "wb") as f:
-        for line in mat:  
-            np.savetxt(f, line, fmt='%.2f')  
-    f.close()
+    # with open(FileName2, "wb") as f:
+    #     for line in mat:  
+    #         np.savetxt(f, line, fmt='%.2f')  
+    # f.close()
     with open(FileName3, "wb") as f:
         for line in mat2:  
             np.savetxt(f, line, fmt='%.2f')  
@@ -170,7 +170,7 @@ def plot(dv_HL, dv_NH, dv_SP, dv_RP, dv_EP):
     color = ['red', 'green', 'blue', 'black', 'orange', 'yellow']
     
     fig = plt.figure(figsize=(12,12), constrained_layout=True)
-    gs = fig.add_gridspec(3, 2)
+    gs = fig.add_gridspec(4,1)
 
     fig.suptitle("Sweep parameter \n Regression Network", fontsize=16)
     
@@ -190,13 +190,13 @@ def plot(dv_HL, dv_NH, dv_SP, dv_RP, dv_EP):
     plt.grid()
     plt.legend(title = "Training splits")
 
-    ax = fig.add_subplot(gs[2,0])
+    ax = fig.add_subplot(gs[2,:])
     plt.plot(dv_EP, te22[:,0], 'x-', c = color[i])
     plt.xlabel('Training epochs')
     plt.ylabel('Training MAE')
     plt.grid()
 
-    ax = fig.add_subplot(gs[2,1])
+    ax = fig.add_subplot(gs[3,:])
     plt.plot(dv_EP, te22[:,1], 'x-', c = color[i])
     plt.xlabel('Training epochs')
     plt.ylabel('Computation time (s)')
@@ -223,12 +223,12 @@ if __name__ == "__main__":
     dv_NH = [3, 5, 10, 20, 50, 80, 100]
     dv_SP = [2, 4, 5, 8, 15]
     dv_RP = [2, 3, 5, 8 ]
-    dv_EP = [10, 20, 30, 50, 100]
+    dv_EP = [10, 20, 30, 50, 100, 150]
 
 
     # ACCURACY
     # optArch(perceptron,  dv_HL, dv_NH)
-    optTra(perceptron, dv_SP, dv_RP, dv_EP)
+    # optTra(perceptron, dv_SP, dv_RP, dv_EP)
     
     plot(dv_HL, dv_NH, dv_SP, dv_RP, dv_EP)
 
